24f5619fa5a3823245cc3cf19f1231eca1eb7cb1
hello434 <76630968+hello434@users.noreply.github.com>
2024-09-21 00:56:52 +0200

    Fixed: tsconfig

--- cypress/e2e/pages/LoginPage/Login/Login.ts
+++ cypress/e2e/pages/LoginPage/Login/Login.ts
@@ -1,6 +1,18 @@
-import { Given } from "@badeball/cypress-cucumber-preprocessor";
-import router from "../../../../routes/common";
+import { Given, Then } from '@badeball/cypress-cucumber-preprocessor';
+import router from '../../../../routes/common';
+import coreUIActions from '../../../core/uiActions';
+import LoginPage from '../Login/LoginPageObjectModel';
 
-Given('TEST', () => {
-    cy.visit('/');
+const coreUIActionsInstance = new coreUIActions();
+const loginPageInstance = new LoginPage();
+
+Given('User opens login page', () => {
+  cy.visit(router.loginpage);
+});
+
+Then('User fills login and password', () => {
+  cy.fixture('standard_user').then((user) => {
+    coreUIActionsInstance.inputType({ el: loginPageInstance.loginPageLoginInput, value: user.username });
+    coreUIActionsInstance.inputType({ el: loginPageInstance.loginPagePasswordInput, value: user.password });
   });
+});
