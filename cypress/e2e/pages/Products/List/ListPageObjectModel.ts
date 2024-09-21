bba717254c8a38eef9f56b387578f058c3b51b03
hello434 <76630968+hello434@users.noreply.github.com>
2024-09-21 22:48:40 +0200

    Fixed: Rename uiGetters

--- cypress/e2e/pages/Products/List/ListPageObjectModel.ts
+++ cypress/e2e/pages/Products/List/ListPageObjectModel.ts
@@ -2,10 +2,10 @@ import coreUI from '../../../core/uiGetters'
 
 export default class ProductsList {
     get listShoppingCart() {
-        return new coreUI().getTestElementByID({el: 'shopping_cart_container'})
+        return new coreUI().getElementByID({el: 'shopping_cart_container'})
     }
 
     getProductTitle({id} : {id: number}){
-        return new coreUI().getTestElementByID({el: `item_${id}_title_link`})
+        return new coreUI().getElementByID({el: `item_${id}_title_link`})
     }
 }
